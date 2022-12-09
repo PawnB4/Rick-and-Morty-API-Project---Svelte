acd96366aab34dc7e207797c9c35cd2a0ec402f4
PawnB4 <francomartinpascual@gmail.com>
2022-12-08 22:39:56 -0300

    Build GH pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/Rick-and-Morty-API-Project---Svelte/'
 })
